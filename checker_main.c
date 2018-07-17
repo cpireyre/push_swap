3fc44f4e17d9b71508b3deeda306dcfb01a79f00
Colin PIREYRE <cpireyre@student.42.fr>
2018-07-17 15:23:40 +0200

    big refactor

--- checker_main.c
+++ checker_main.c
@@ -6,7 +6,7 @@
 /*   By: cpireyre <cpireyre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/13 09:36:13 by cpireyre          #+#    #+#             */
-/*   Updated: 2018/07/17 14:46:40 by cpireyre         ###   ########.fr       */
+/*   Updated: 2018/07/17 14:51:08 by cpireyre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,31 +39,34 @@ t_wheel		*init_wheel(int argc, char **argv)
 	return (wheel);
 }
 
+t_bool	checker(t_wheel **to_sort, t_wheel **reserve)
+{
+	char	*line;
+
+	line = NULL;
+	while (ft_gnl(0, &line))
+	{
+		do_action(to_sort, reserve, line);
+		*to_sort = wheel_go_to_head(*to_sort);
+		*reserve = wheel_go_to_head(*reserve);
+		free(line);
+	}
+	return (check_wheel_sortedness(*to_sort));
+}
+
 int		main(int argc, char **argv)
 {
 	t_wheel	*to_sort;
-	t_wheel *b;
-	char	*line;
+	t_wheel *reserve;
 
-	b = NULL;
-	line = NULL;
+	reserve = NULL;
 	to_sort = (init_wheel(argc, argv));
-	/* later:
-	 ** extract this loop into a function returns the sortedness?
-	 */
-	while (ft_gnl(0, &line))
-	{
-		do_action(&to_sort, &b, line);
-		to_sort = wheel_go_to_head(to_sort);
-		b = wheel_go_to_head(b);
-		free(line);
-	}
-	if (to_sort && check_wheel_sortedness(to_sort) == true)
+	if (checker(&to_sort, &reserve) == true)
 		ft_putendl("OK");
-	else	
+	else
 		ft_putendl("KO");
 	wheel_free_all(to_sort);
-	if (b)
-		wheel_free_all(b);
+	if (reserve)
+		wheel_free_all(reserve);
 	return (0);
 }
