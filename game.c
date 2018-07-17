3ed31e2095877f9235d92c75b34bafa93130ba8f
Colin PIREYRE <cpireyre@student.42.fr>
2018-07-17 09:25:24 +0200

    print number of moves at the end

--- game.c
+++ game.c
@@ -6,7 +6,7 @@
 /*   By: cpireyre <cpireyre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/16 11:59:19 by cpireyre          #+#    #+#             */
-/*   Updated: 2018/07/17 08:52:54 by cpireyre         ###   ########.fr       */
+/*   Updated: 2018/07/17 09:19:42 by cpireyre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,9 +35,11 @@ void	curse_print_wheel(t_wheel *wheel, int x, int y)
 void	wrap_ncurses(t_wheel **a, t_wheel **b)
 {
 	int		keypress;
+	int		moves;
 
 	curse_print_wheel(*a, 5, 3);
 	mvprintw(15, 1, "q to quit");
+	moves = 0;
 	while ((keypress = getch()) != 'q')
 	{
 		clear();
@@ -58,10 +60,11 @@ void	wrap_ncurses(t_wheel **a, t_wheel **b)
 			printw("%c", keypress);
 		curse_print_wheel(*a, X_A, 3);
 		curse_print_wheel(*b, X_B, 3);
+		moves++;
 	}
 	if (*a && check_wheel_sortedness(*a) == true && !*b)
 	{
-		mvprintw(10, 10, "Well done!");
+		mvprintw(10, 10, "Well done! %d moves.", moves);
 		getch();
 	}
 	endwin();
