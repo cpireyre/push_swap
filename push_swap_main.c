d6fedbe0c122bedcfa6b8b3f4bfe7e879cc57f94
Colin PIREYRE <cpireyre@student.42.fr>
2018-08-13 14:50:49 +0200

    added dirty special case for n=2

--- push_swap_main.c
+++ push_swap_main.c
@@ -6,7 +6,7 @@
 /*   By: cpireyre <cpireyre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/13 13:32:32 by cpireyre          #+#    #+#             */
-/*   Updated: 2018/08/10 14:00:21 by cpireyre         ###   ########.fr       */
+/*   Updated: 2018/08/13 14:50:26 by cpireyre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,17 +24,27 @@ void	push_swap(t_ps *ps)
 		do_and_print(ps->a, ps->b, "pa");
 }
 
+static void	swap_pair(t_ps *ps)
+{
+	if ((*ps->a)->number > (*ps->a)->next->number)
+		do_and_print(ps->a, ps->b, "sa");
+}
+
 int		main(int argc, char **argv)
 {
 	t_wheel	*a;
+	t_wheel	*b;
 	t_ps	*ps;
 
 	a = init_wheel(argc, argv);
-	ps = init_ps(a);
-	wheel_add_tail((ps->b), 214);
-	ft_printf("%d", (*(ps->b))->number);
-	wheel_print_spoke(*(ps->b));
-	//push_swap(ps);
+	b = NULL;
+	ps = init_ps(&a, &b);
+	if (!ps)
+		free_and_quit(a, b);
+	if (ps->total_size == 2)
+		swap_pair(ps);
+	else
+		push_swap(ps);
 	wheel_free_all(a);
 	return (0);
 }
