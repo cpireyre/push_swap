a4915339d4ec258c6020d4004d28c0a22bf2ed9d
cpireyre <cpireyre@student.42.fr>
2018-09-15 21:20:16 +0200

    still working on it

--- quicksort.c
+++ quicksort.c
@@ -1,6 +1,6 @@
 #include "header.h"
 
-t_bool	sorted_up_to_cutoff(t_ps *ps, t_byte sort, int cutoff)
+t_bool	sorted_up_to_rank(t_ps *ps, t_byte sort, int rank)
 {
 	int	i;
 	int	step;
@@ -9,7 +9,7 @@ t_bool	sorted_up_to_cutoff(t_ps *ps, t_byte sort, int cutoff)
 	i = 0;
 	tape = sort ? A : B;
 	step = sort ? -1 : 1;
-	while (tape[++i] && i < cutoff)
+	while (tape[++i] && i < rank)
 		if (tape[i] != tape[i - 1] + step)
 			return (false);
 	return (true);
@@ -27,29 +27,42 @@ void	a_push_up(t_ps *ps, t_bool will_print)
 	RA;
 }
 
+void	get_patterns(t_pattern *toby, t_byte sort)
+{
+	toby[0] = sort ? &pb : &pa;
+	toby[1] = sort ? &b_push_up : &a_push_up;
+	toby[2] = sort ? &ra : &rb;
+	toby[3] = sort ? &rrb : &rra;
+	toby[4] = sort ? &rra : &rrb;
+}
+
 void	two_tapes_quicksort(t_ps *ps, int cutoff, int size, t_byte sort)
 {
 	int	i;
 	int	*tape;
-	t_pattern	push;
-	t_pattern	push_up;
-	t_pattern	spin;
-	t_pattern	rspin;
+	t_pattern	toby[5];
 	
 	tape = sort ? A : B;
-	push = sort ? &pb : &pa;
-	push_up = sort ? &b_push_up : &a_push_up;
-	spin = sort ? &ra : &rb;
-	rspin = sort ? &rrb : &rra;
+	if (sorted_up_to_rank(ps, sort, size))
+		return ;
+	get_patterns(toby, sort);
 	i = size;
 	while (i--)
 	{
 		if (tape[0] > cutoff)
-			push(ps, PRINT);
+			toby[0](ps, PRINT);
 		else if (tape[0] == cutoff)
-			push_up(ps, PRINT);
+			toby[1](ps, PRINT);
 		else
-			spin(ps, PRINT);
+			toby[2](ps, PRINT);
 	}
-	rspin(ps, PRINT);
+	toby[3](ps, PRINT);
+	while (size--)
+	{
+		if (tape[0] < cutoff)
+			toby[0](ps, PRINT);
+		else
+			toby[4](ps, PRINT);
+	}
+	two_tapes_quicksort(ps, cutoff / 2, size / 2, sort ? 0 : 1);
 }
