f1939e2cb417c85313eae661923a46b962128720
Colin PIREYRE <cpireyre@student.42.fr>
2018-09-18 12:13:20 +0200

    crappy norm fix

--- draw.c
+++ draw.c
@@ -6,24 +6,16 @@
 /*   By: cpireyre <cpireyre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/05 09:52:54 by cpireyre          #+#    #+#             */
-/*   Updated: 2018/09/14 10:06:45 by cpireyre         ###   ########.fr       */
+/*   Updated: 2018/09/18 12:11:43 by cpireyre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
 #include <unistd.h>
 
-void	ncurses_print_tab(int *tab, int *sorted, int size, size_t leftshift, int total)
-{
-	int		i;
-
-	i = -1;
-	while (++i < size)
-		mvprintw(total - i, leftshift, "%d", sorted[tab[size - i - 1] - 1]);
-}
-
 void	visu_wrapper(t_ps *ps, char **line, int *sorted)
 {
+	int	i;
 	int	height;
 	int	size_a;
 	int	size_b;
@@ -33,15 +25,19 @@ void	visu_wrapper(t_ps *ps, char **line, int *sorted)
 	height = size_a + size_b;
 	clear();
 	mvprintw(height + 2, 1, "-------------------||-------------------");
+	i = -1;
 	if (size_a)
-		ncurses_print_tab(A, sorted, size_a, 1, height);
+		while (++i < size_a)
+			mvprintw(height - i, 3, "%d", sorted[A[size_a - i - 1] - 1]);
 	mvprintw(height + 3, 9, "a");
+	i = -1;
 	if (size_b)
-		ncurses_print_tab(B, sorted, size_b, 30, height);
+		while (++i < size_b)
+			mvprintw(height - i, 32, "%d", sorted[B[size_b - i - 1] - 1]);
 	mvprintw(height + 3, 30, "b");
 	refresh();
 	do_action(ps, line, NO_PRINT);
-	size_a = 900000000;
+	size_a = 100000000;
 	while (size_a--)
 		;
 }
