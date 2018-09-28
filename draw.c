090e6280ac9058a3aa7e5c5f216d625f04428948
cpireyre <cpireyre@student.42.fr>
2018-09-28 02:31:58 +0200

    refactor + added colors

--- draw.c
+++ draw.c
@@ -13,35 +13,57 @@
 #include "header.h"
 #include <unistd.h>
 
-void	ncurses_print_tab(int *tab, int *sorted, int size, size_t leftshift, int total)
+typedef struct	s_draw
+{
+	int	*tape;
+	int	size;
+	int	color;
+}		t_draw;
+
+t_draw	create_draw_struct(int *tape, int color)
+{
+	t_draw	ret;
+
+	ret.tape = tape;
+	ret.size = tablen(tape);
+	ret.color = color;
+	return (ret);
+}
+
+void	ncurses_print_tab(t_draw to_draw, int *sorted, size_t leftshift, int total)
 {
 	int		i;
 
 	i = -1;
-	while (++i < size)
-		mvprintw(total - i, leftshift, "%d", sorted[tab[size - i - 1] - 1]);
+	attron(COLOR_PAIR(to_draw.color));
+	while (++i < to_draw.size)
+		mvprintw(total - i, leftshift, "%d", \
+				sorted[to_draw.tape[to_draw.size - i - 1] - 1]);
+	attroff(COLOR_PAIR(to_draw.color));
 }
 
 void	visu_wrapper(t_ps *ps, char **line, int *sorted)
 {
+	t_draw	tape_a;
+	t_draw	tape_b;
 	int	height;
-	int	size_a;
-	int	size_b;
 
-	size_a = tablen(A);
-	size_b = tablen(B);
-	height = size_a + size_b;
+	init_pair(1, COLOR_RED, -1);
+	init_pair(2, COLOR_BLUE, -1);
+	tape_a = create_draw_struct(A, 1);
+	tape_b = create_draw_struct(B, 2);
+	height = tape_a.size + tape_b.size;
 	clear();
 	mvprintw(height + 2, 1, "-------------------||-------------------");
-	if (size_a)
-		ncurses_print_tab(A, sorted, size_a, 1, height);
+	if (tape_a.size)
+		ncurses_print_tab(tape_a, sorted, 1, height);
 	mvprintw(height + 3, 9, "a");
-	if (size_b)
-		ncurses_print_tab(B, sorted, size_b, 30, height);
+	if (tape_b.size)
+		ncurses_print_tab(tape_b, sorted, 30, height);
 	mvprintw(height + 3, 30, "b");
 	refresh();
 	do_action(ps, line, NO_PRINT);
-	size_a = 500000000;
-	while (size_a--)
+	height = 200000000;
+	while (height--)
 		;
 }
